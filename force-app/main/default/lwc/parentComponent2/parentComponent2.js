a4e2b1423e0226f143bdef8194db3cad81b57cfa
Sagar Sahore <65637133+sagarsahore@users.noreply.github.com>
2025-08-12 02:08:27 +0000

    update: practice question 1

--- force-app/main/default/lwc/parentComponent2/parentComponent2.js
+++ force-app/main/default/lwc/parentComponent2/parentComponent2.js
@@ -1,3 +1,9 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class ParentComponent2 extends LightningElement {}
+export default class ParentComponent2 extends LightningElement {
+    @track msg;
+    handleCustomEvent(event){
+        this.msg = event.detail;
+    }
+
+}
